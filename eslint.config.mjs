a1a2a98da6ab4d6bfd93ea870678afafcda77cf9
renato3x <pereirarenato21@gmail.com>
2025-04-13 13:50:38 -0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,71 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  {
+    files: ['**/*.ts'],
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    settings: {
+      prettier: {
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        tabWidth: 2,
+        printWidth: 120,
+      },
+    },
+  },
+  {
+    rules: {
+      quotes: ['error', 'single', { avoidEscape: true }],
+      eqeqeq: ['error', 'always'],
+      semi: ['error', 'always'],
+      curly: ['error', 'all'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'eol-last': ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'no-template-curly-in-string': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          trailingComma: 'all',
+          tabWidth: 2,
+          printWidth: 120,
+        },
+      ],
+    },
+  },
+];
---
+++ src/@types/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    RMQ_URL: string;
+    MESSAGE_BROKER_QUEUE_NAME: string;
+  }
+}
