1a34bc0fc25142f1c15b33ca116bcb2ac895ce16
renato3x <pereirarenato21@gmail.com>
2025-04-15 21:29:56 -0300

    chore: read env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule]
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
 })
 export class AppModule {}
